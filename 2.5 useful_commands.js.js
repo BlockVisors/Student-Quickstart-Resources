2e4132a20113f7dbfc4e370913d73472ba2aee26
BlockVisors <40542573+BlockVisors@users.noreply.github.com>
2018-09-08 20:57:01 -0400

    Add files via upload

---
+++ 2.5 useful_commands.js.js
@@ -0,0 +1,29 @@
+//check node version
+node -v 
+
+//check mpm version
+npm -v
+
+//install truffle
+npm install -g truffle
+
+//initialize truffle
+truffle init
+
+//initialize node js project nd creacte package.json file
+npm init -y
+
+//install zeppelin-solidity library
+npm install -E zeppelin-solidity
+
+//create directory
+mkdir <path or name>
+
+//go to directory
+cd <path or name>
+
+//get current directory
+pwd
+
+//list of files and directories in current directory
+ls
---
+++ 4.1 useful_commands2.js.js
@@ -0,0 +1,14 @@
+//install Remixd
+npm install -g remixd
+
+//share your local folder to Remix IDE
+remixd -s <absolute-path-to-the-shared-folder> 
+
+//check absolute path
+pwd
+
+//list directory files and folders
+ls
+
+//change directory
+cd <directory> 
